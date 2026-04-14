a5ffde5e6a9df8dcd34ee429ebdc95124ab569cc
Luna S <>
2026-04-14 13:13:03 +0200

    codigo inicial

---
+++ src/main/java/EjercicioED/EjercicioEntornos/App.java
@@ -0,0 +1,10 @@
+package EjercicioED.EjercicioEntornos;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
