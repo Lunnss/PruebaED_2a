f3797c2eaca18dc4b274e1c4a557dfecbbf85b47
Luna S <>
2026-04-14 13:33:56 +0200

    documentada Curso

--- src/main/java/EjercicioED/EjercicioEntornos/Curso.java
+++ src/main/java/EjercicioED/EjercicioEntornos/Curso.java
@@ -7,23 +7,38 @@ package EjercicioED.EjercicioEntornos;
 import java.util.TreeSet;
 
 /**
- *
- * @author ProfDiurno
+ * @author Luna
+ * @version 1.0
  */
 public class Curso {
 
     private String nombre;
     private TreeSet<Persona> listaAlumnos;
 
+    /**
+     * Devuelve el nombre del curso.
+     *
+     * @return nombre del curso
+     */
     protected String getNombre() {
         return nombre;
     }
 
+    /**
+     * Constructor de la clase Curso.
+     * Inicializa el nombre del curso y la lista de alumnos.
+     */
     public Curso(String nombre) {
         this.nombre = nombre;
         listaAlumnos = new TreeSet<>();
     }
 
+    /**
+     * Devuelve una representación en texto del curso
+     * incluyendo la lista de alumnos.
+     *
+     * @return cadena con los datos del curso
+     */
     @Override
     public String toString() {
         String s = "--------------------" + nombre + "-----------------\n";
@@ -35,6 +50,9 @@ public class Curso {
         return s;
     }
 
+    /**
+     * Añade un alumno al curso.
+     */
     public void aniadirAlumno(Persona p) {
         listaAlumnos.add(p);
     }
